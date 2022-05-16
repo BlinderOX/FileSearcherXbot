cab9378f7d96764561533f4941b5e1fe0bd51926
BlinderOX <102313067+BlinderOX@users.noreply.github.com>
2022-05-16 15:09:30 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -331,7 +331,7 @@ async def bot_info(bot, message):
     buttons = [
         [
             
-            InlineKeyboardButton('⚙️ Bot Updates Channel ⚙️', url=f'{TUTORIAL}')
+            InlineKeyboardButton('⚙️Bot Updates Channel⚙️', url=f'{TUTORIAL}')
         ]
         ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
